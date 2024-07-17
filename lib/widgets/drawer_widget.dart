43a63082572b13afe1965c5ce3ddb5d42d20f4cb
songjunha <likesong00@naver.com>
2024-07-17 20:29:27 +0900

    commit

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-import '../views/userinfo_screen.dart';
 import '../views/workout_learning_screen.dart';
 
 
